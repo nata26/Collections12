b223a8faf744e4d71cb4f9253d876ec68953cf3c
Natalia Mocanu <nmocanu@endava.com>
2017-03-15 17:25:03 +0200

    Initial commit

---
+++ PersonMain.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class PersonMain {
+
+    public static void main(String[] args){
+
+        PersonArray p = new PersonArray();
+        p.insert("Smith","John",40);
+        p.display();
+        p.insert("Mocanu","Nata",21);
+        p.display();
+        p.insert("Mac","Daniel",35);
+        p.display();
+        p.insert("Jobs","Steve",60);
+        p.display();
+        p.insert("Donald","Stew",38);
+        p.display();
+
+
+
+
+
+    }
+}
