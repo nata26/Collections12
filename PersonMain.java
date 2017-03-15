6432601c02cf8845379e44361b79172d6cabc9cf
Natalia Mocanu <nmocanu@endava.com>
2017-03-15 17:48:31 +0200

    two commit

--- PersonMain.java
+++ PersonMain.java
@@ -2,21 +2,47 @@ import java.util.ArrayList;
 
 public class PersonMain {
 
+    public static void instanceAll(Person[] p){
+        for (int i = 0; i< p.length; i++)
+            p[i] = new Person();
+    }
+    public static void insert(Person[] p){
+
+        for (int i = 0; i< p.length; i++){
+            p[0].setFirstName("Sff");
+            p[0].setLastName("Joff");
+            p[0].setAge(90);
+            p[1].setFirstName("Smith");
+            p[1].setLastName("John");
+            p[1].setAge(40);
+            p[2].setFirstName("jjj");
+            p[2].setLastName("lll");
+            p[2].setAge(90);
+            p[3].setFirstName("aaa");
+            p[3].setLastName("aaa");
+            p[3].setAge(40);
+        }
+    }
+
+    public static void display(Person[] p){
+        for (int i = 0; i< p.length; i++){
+            System.out.println("First name: " + p[i].getFirstName() + "\n" +
+                    "Last name: " + p[i].getLastName() +"\n" +
+                    "Age: " + p[i].getAge() + "\n*********************************");}
+
+
+
+    }
+
+
+
+
     public static void main(String[] args){
 
-        PersonArray p = new PersonArray();
-        p.insert("Smith","John",40);
-        p.display();
-        p.insert("Mocanu","Nata",21);
-        p.display();
-        p.insert("Mac","Daniel",35);
-        p.display();
-        p.insert("Jobs","Steve",60);
-        p.display();
-        p.insert("Donald","Stew",38);
-        p.display();
-
-
+        Person[] p = new Person[4];
+        instanceAll(p);
+        insert(p);
+        display(p);
 
 
 
