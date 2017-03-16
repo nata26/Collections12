fe17b0fe992af0ddd561d7743a091df375e8b5ab
nmocanu <Natalia.Mocanu@endava.com>
2017-03-16 14:42:31 +0200

    changes

--- PersonMain.java
+++ PersonMain.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class PersonMain {
 
     public static void instanceAll(Person[] p){
@@ -9,18 +7,18 @@ public class PersonMain {
     public static void insert(Person[] p){
 
         for (int i = 0; i< p.length; i++){
-            p[0].setFirstName("Sff");
-            p[0].setLastName("Joff");
-            p[0].setAge(90);
+            p[0].setFirstName("Steve");
+            p[0].setLastName("Jobs");
+            p[0].setAge(60);
             p[1].setFirstName("Smith");
             p[1].setLastName("John");
-            p[1].setAge(40);
-            p[2].setFirstName("jjj");
-            p[2].setLastName("lll");
-            p[2].setAge(90);
-            p[3].setFirstName("aaa");
-            p[3].setLastName("aaa");
-            p[3].setAge(40);
+            p[1].setAge(45);
+            p[2].setFirstName("Mocanu");
+            p[2].setLastName("Nata");
+            p[2].setAge(21);
+            p[3].setFirstName("Colins");
+            p[3].setLastName("Ariel");
+            p[3].setAge(36);
         }
     }
 
@@ -30,10 +28,41 @@ public class PersonMain {
                     "Last name: " + p[i].getLastName() +"\n" +
                     "Age: " + p[i].getAge() + "\n*********************************");}
 
+    }
+
+    public static int find (Person[] p, String LastName){
+
+       int index = -1;
+       for (int i=0; i<p.length;i++)
+        if (p[i].getLastName().equals(LastName)){
+           index = i;
+            System.out.println("Elementul este pe pozitia: " + i);
+            break;
+        } else {
+            System.out.println("Next!");
+        }
+
+        return index;
+    }
+
+    public static void delete(Person[] p, String LastName,int index){
+        System.out.println(index);
+
+        int size = 4;
+        int poz = index;
+        Person[] p1 = new Person[size];
+        for (int i = 0; i < size; i++){
+            if (i == poz) continue;
+            p1[i]=p[i];
+        System.out.println(p1[i].getLastName());
+        }
+
+
+
+
 
 
     }
-
 
 
 
@@ -43,6 +72,9 @@ public class PersonMain {
         instanceAll(p);
         insert(p);
         display(p);
+        //find(p,"John");
+        delete(p, "John", find(p, "John"));
+
 
 
 
